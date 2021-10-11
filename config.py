632ed20fb63adbb4b9297bcb3074d0cd8ff4e124
Carson B Dunlap <carson.b.dunlap@gmail.com>
2021-10-11 09:11:49 -0400

    commit 1

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+TOKEN = os.environ.get("TOKEN", "2071736077:AAGUIMlucdV4u1ZBrCXtg_hxbkuw21NqSzw")
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "WYAHRW-RPGVQE-LRDKXP-MQXRGN-ARQ")
+REPO_BOT = os.environ.get("REPO_BOT", "https://github.com/carsond135/PornHub")
+OWNER = os.environ.get("OWNER", "cbd94")
+BOT_NAME = os.environ.get("BOT_NAME", "PHub Downloader Bot")
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
