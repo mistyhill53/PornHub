c5e2a4ef10534387aabf431b678459a5ae5e8e88
mistyhill53 <58066607+mistyhill53@users.noreply.github.com>
2021-12-24 21:28:47 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 TOKEN = os.environ.get("TOKEN", "5021406619:AAH4ZVdk26MTA6LGC0MBi1LLzHxSTaBHjaU")
-ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "WYAHRW-RPGVQE-LRDKXP-MQXRGN-ARQ")
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "OUGLSA-APQOTL-GNBAPX-BOHMMU-ARQ")
 REPO_BOT = os.environ.get("REPO_BOT", "https://github.com/linux-jin/PornHub")
 OWNER = os.environ.get("OWNER", "mistyhill")
 BOT_NAME = os.environ.get("BOT_NAME", "PHub Downloader Bot")
